0747a7fb34879c8b370d6b6fe5bb964fd44bd231
Andreas Paffenholz <paffenholz@mathematik.tu-darmstadt.de>
2014-03-06 21:02:15 +0100

    changed predicate prefix

--- scripts/rdf_smoothreflexive.pl
+++ scripts/rdf_smoothreflexive.pl
@@ -54,10 +54,10 @@ EOF
 
 sub getPreamble {
     return <<'EOF';
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix owl:  <http://www.w3.org/2002/07/owl#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+@prefix xsd:  <http://www.w3.org/2001/XMLSchema#> .
 
 @prefix pm: <http://polymake.org/data/> .
 
